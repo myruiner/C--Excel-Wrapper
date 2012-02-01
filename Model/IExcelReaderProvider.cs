40e27259b549e903b77b8d439580d010c6ff3c04
myruiner <pascal.thuet@gmail.com>
2012-02-01 17:50:06 +0100

    Still working

---
+++ Model/IExcelReaderProvider.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IExcelReaderProvider.cs" company="Trivadis AG">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface IExcelReaderProvider
+    {
+    }
+}
